e367d7c0db5148645c672b1961e3074eb806de73
Stratos Pavlakis <pavlakis@workable.com>
2014-07-29 11:26:42 +0300

    Add intro tutorial for Kendo app

--- kendo/app/assets/javascripts/mobile/app.js
+++ kendo/app/assets/javascripts/mobile/app.js
@@ -1,3 +1,7 @@
 var myApp = myApp || {};
 
-myApp.app = new kendo.mobile.Application(document.body, { transition: "slide", skin: 'ios' });
+myApp.app = new kendo.mobile.Application(document.body, {
+  skin: 'flat',
+  layout: 'main-layout',
+  initial: 'home'
+});
